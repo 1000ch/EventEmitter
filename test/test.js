d180469c01ede47ba314ea8826ab1ab12ebc92f7
1000ch <orz1000ch@gmail.com>
2013-12-25 20:01:56 +0900

    add tests

--- test/test.js
+++ test/test.js
@@ -13,15 +13,36 @@ describe('getListeners', function() {
   });
   
   it('contains a listener', function() {
-    ee.addListener('event1', function() {
-      console.log('event1 is fired');
-    });
+    ee.addListener('event1', function() {});
     expect(ee.getListeners('event1').length).to.equal(1);
+  });
+
+  it('contains some listeners', function() {
+    ee.addListener('event2', function() {});
+    ee.addListener('event2', function() {});
+    expect(ee.getListeners('event2').length).to.equal(2);
+  });
+  
+  it('contains some listeners with other listeners', function() {
+    ee.addListener('event3', function() {});
+    ee.addListener('event3', function() {});
+    ee.addListener('event3', function() {});
+    ee.addListener('event4', function() {});
+    ee.addListener('event4', function() {});
+    expect(ee.getListeners('event3').length).to.equal(3);
   });
 });
 
 describe('addListener', function() {
+  var ee;
   
+  beforeEach(function() {
+    ee = new EventEmitter();
+  });
+  
+  it('adds a listener', function() {});
+  it('adds same listeners', function() {});
+  it('adds some type listeners', function() {});
 });
 
 describe('addOnceListener', function() {
